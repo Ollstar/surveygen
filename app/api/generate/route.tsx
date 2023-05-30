9defd13fe58236110284830495df5fcffbf68929
Ollstar <oltandrews@gmail.com>
2023-05-30 16:51:35 -0700

    initial template testing

--- app/api/generate/route.tsx
+++ app/api/generate/route.tsx
@@ -1,61 +1,89 @@
 import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { LLMChain } from 'langchain/chains';
+import { LLMChain, SimpleSequentialChain } from 'langchain/chains';
 import { CallbackManager } from 'langchain/callbacks';
 import {
   ChatPromptTemplate,
   HumanMessagePromptTemplate,
+  PromptTemplate,
 } from 'langchain/prompts';
 
-const prompt = ChatPromptTemplate.fromPromptMessages([
-  HumanMessagePromptTemplate.fromTemplate('{input}'),
-]);
+// Define the PromptTemplates for the playwright and critic scenarios.
+const playwrightTemplate = `You are a playwright. Given the {input}, it is your job to write a synopsis for that title.
+Title: {input}
+Playwright: This is a synopsis for the above play:`;
+const criticTemplate = `You are a play critic from the New York Times. Given the synopsis of play, it is your job to write a review for that play.
+Play Synopsis:
+{synopsis}
+Review from a New York Times play critic of the above play:`;
+const playwrightPromptTemplate = new PromptTemplate({ template: playwrightTemplate, inputVariables: ["input"] });
+const criticPromptTemplate = new PromptTemplate({ template: criticTemplate, inputVariables: ["synopsis"] });
 
 export async function POST(req: Request) {
   try {
     const { input } = await req.json();
-    console.log('input', input);
+
     // Check if the request is for a streaming response.
     const streaming = req.headers.get('accept') === 'text/event-stream';
-    console.log('server streaming', streaming);
+
     if (streaming) {
       // For a streaming response we need to use a TransformStream to
       // convert the LLM's callback-based API into a stream-based API.
       const encoder = new TextEncoder();
       const stream = new TransformStream();
       const writer = stream.writable.getWriter();
-      console.log('creating llm');
-      const llm = new ChatOpenAI({
-        streaming,
-        callbackManager: CallbackManager.fromHandlers({
-          handleLLMNewToken: async (token: string) => {
-            await writer.ready;
-            await writer.write(encoder.encode(`data: ${token}\n\n`));
-          },
-          handleLLMEnd: async () => {
-            await writer.ready;
-            await writer.close();
-          },
-          handleLLMError: async (e: Error) => {
-            await writer.ready;
-            await writer.abort(e);
-          },
-        }),
+      
+      const callbackManager = CallbackManager.fromHandlers({
+        handleLLMNewToken: async (token: string) => {
+          await writer.ready;
+          await writer.write(encoder.encode(`data: ${token}\n\n`));
+        },
+        handleLLMEnd: async () => {
+          await writer.ready;
+          await writer.close();
+        },
+        handleLLMError: async (e: Error) => {
+          await writer.ready;
+          await writer.abort(e);
+        },
       });
-      console.log('creating chain');
-      const chain = new LLMChain({ prompt, llm });
-      // We don't need to await the result of the chain.run() call because
-      // the LLM will invoke the callbackManager's handleLLMEnd() method
-      chain.call({ input }).catch((e: Error) => console.error(e));
-      console.log('returning response');
+
+      // Create the LangChain instances for playwright and critic
+      const playwrightLLM = new ChatOpenAI({ streaming, callbackManager });
+      const criticLLM = new ChatOpenAI({ streaming, callbackManager });
+
+      // Create the LangChain instances for playwright and critic
+      const playwrightChain = new LLMChain({ prompt: playwrightPromptTemplate, llm: playwrightLLM });
+      const criticChain = new LLMChain({ prompt: criticPromptTemplate, llm: criticLLM });
+
+      // Creating the overall chain
+      const overallChain = new SimpleSequentialChain({
+        chains: [playwrightChain, criticChain],
+        verbose: true,
+      });
+
+      // Run the overall chain
+      await overallChain.run(input);
+
+      // Return the readable side of the TransformStream as the HTTP response.
       return new Response(stream.readable, {
         headers: { 'Content-Type': 'text/event-stream' },
       });
     } else {
       // For a non-streaming response we can just await the result of the
       // chain.run() call and return it.
-      const llm = new ChatOpenAI();
-      const chain = new LLMChain({ prompt, llm });
-      const response = await chain.call({ input });
+      const playwrightLLM = new ChatOpenAI();
+      const criticLLM = new ChatOpenAI();
+
+      const playwrightChain = new LLMChain({ prompt: playwrightPromptTemplate, llm: playwrightLLM });
+      const criticChain = new LLMChain({ prompt: criticPromptTemplate, llm: criticLLM });
+
+      // Creating the overall chain
+      const overallChain = new SimpleSequentialChain({
+        chains: [playwrightChain, criticChain],
+        verbose: true,
+      });
+
+      const response = await overallChain.run(input);
 
       return new Response(JSON.stringify(response), {
         headers: { 'Content-Type': 'application/json' },
