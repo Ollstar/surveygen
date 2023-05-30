3f52b14a76f711f64d5ef39ce121fa0445cc6780
Ollstar <oltandrews@gmail.com>
2023-05-30 14:48:15 -0700

    use flex grow for message view

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export const runtime = "edge";
 
 interface Message {
   id: string;
-  sender: 'user' | 'bot';
+  sender: "user" | "bot";
   content: string;
 }
 
@@ -23,7 +23,11 @@ const Chat: React.FC = () => {
 
       if (inflight) return;
 
-      const userMessage: Message = { id: Date.now().toString(), sender: 'user', content: input };
+      const userMessage: Message = {
+        id: Date.now().toString(),
+        sender: "user",
+        content: input,
+      };
       setMessages((prev) => [...prev, userMessage]);
 
       setInflight(true);
@@ -31,7 +35,7 @@ const Chat: React.FC = () => {
       try {
         if (stream) {
           let botMessage = "";
-          let isFirstChunk = true; 
+          let isFirstChunk = true;
           await fetchEventSource(`/api/generate`, {
             method: "POST",
             body: JSON.stringify({ input }),
@@ -39,14 +43,20 @@ const Chat: React.FC = () => {
             onmessage(ev) {
               const newChunk = ev.data;
               if (isFirstChunk) {
-                const botMessage: Message = { id: Date.now().toString(), sender: 'bot', content: newChunk };
+                const botMessage: Message = {
+                  id: Date.now().toString(),
+                  sender: "bot",
+                  content: newChunk,
+                };
                 setMessages((prevMessages) => [...prevMessages, botMessage]);
                 isFirstChunk = false;
               } else {
                 botMessage += newChunk;
                 setMessages((prevMessages) => {
                   const updatedMessages = prevMessages.map((message) => {
-                    if (message.id === prevMessages[prevMessages.length - 1].id) {
+                    if (
+                      message.id === prevMessages[prevMessages.length - 1].id
+                    ) {
                       return { ...message, content: botMessage };
                     }
                     return message;
@@ -63,7 +73,11 @@ const Chat: React.FC = () => {
             body: JSON.stringify({ input }),
           });
           const data = await res.json();
-          const newMessage: Message = { id: Date.now().toString(), sender: 'bot', content: data.text };
+          const newMessage: Message = {
+            id: Date.now().toString(),
+            sender: "bot",
+            content: data.text,
+          };
           setMessages((prevMessages) => [...prevMessages, newMessage]);
           setInput("");
         }
@@ -81,12 +95,16 @@ const Chat: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto max-w-2xl border rounded">
-
-      <div className="relative w-full p-6 overflow-y-auto h-[40rem]">
+    <div className="container mx-auto max-w-2xl border rounded min-h-screen flex flex-col">
+      <div className="relative w-full p-6 overflow-y-auto flex-grow">
         <ul className="space-y-2">
-          {messages.map(message => (
-            <li key={message.id} className={`flex ${message.sender === 'user' ? 'justify-end' : 'justify-start'}`}>
+          {messages.map((message) => (
+            <li
+              key={message.id}
+              className={`flex ${
+                message.sender === "user" ? "justify-end" : "justify-start"
+              }`}
+            >
               <div className="relative max-w-xl px-4 py-2 text-gray-600 bg-white rounded-md shadow-md">
                 {message.content}
               </div>
@@ -94,7 +112,10 @@ const Chat: React.FC = () => {
           ))}
         </ul>
       </div>
-      <form onSubmit={onSubmit} className="relative flex items-center p-3 border-t border-gray-300">
+      <form
+        onSubmit={onSubmit}
+        className="relative flex items-center p-3 border-t border-gray-300"
+      >
         <input
           type="text"
           value={input}
@@ -102,7 +123,10 @@ const Chat: React.FC = () => {
           className="w-full h-10 px-3 py-2 text-gray-700 placeholder-gray-300 border rounded-md focus:outline-none"
           placeholder="Type a message"
         />
-        <button type="submit" className="ml-2 px-4 py-2 text-white bg-blue-500 rounded-full shadow-md hover:bg-blue-600 focus:outline-none">
+        <button
+          type="submit"
+          className="ml-2 px-4 py-2 text-white bg-blue-500 rounded-full shadow-md hover:bg-blue-600 focus:outline-none"
+        >
           Send
         </button>
       </form>
